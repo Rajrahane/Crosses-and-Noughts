4eef1d4c4d55f8bb5a40980bbdd366dffa7fb65c
Raj Rahane <rajrahane10399@gmail.com>
2018-07-13 12:05:47 +0530

    First Successful Game

--- app/src/main/java/com/project/raj/tictactoe/GameActivity.java
+++ app/src/main/java/com/project/raj/tictactoe/GameActivity.java
@@ -2,12 +2,74 @@ package com.project.raj.tictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.Arrays;
+
+import TicTacToe.Board;
 
 public class GameActivity extends AppCompatActivity {
-
+    Button GameMoveButton[]=new Button[9];
+    GameUI newGame=new GameUI();
+    private static final int[] BUTTON_IDS={
+            R.id.btn_board0,
+            R.id.btn_board1,
+            R.id.btn_board2,
+            R.id.btn_board3,
+            R.id.btn_board4,
+            R.id.btn_board5,
+            R.id.btn_board6,
+            R.id.btn_board7,
+            R.id.btn_board8
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        init();
+        setUpListeners();
+    }
+
+    void setUpListeners() {
+        int i;
+        for(i=0;i<GameMoveButton.length;i++){
+            GameMoveButton[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Button callingButton=(Button)view;
+                    callingButton.setEnabled(false);
+                    callingButton.setText("O");//change button settings here
+                    int movePlayedByAI=newGame.playMove(findIndexOf(callingButton));
+                    if(newGame.getTurn()!= Board.TOTALMOVES+1){
+                        GameMoveButton[movePlayedByAI].setEnabled(false);
+                        GameMoveButton[movePlayedByAI].setText("X");
+                    }    //valid move was played by ai
+                    if(newGame.evaluateGame()==Board.COMPUTERWINS){     //ai wins
+                        Toast.makeText(view.getContext(),"You Lose",Toast.LENGTH_SHORT).show();
+                    }
+                    else if(newGame.evaluateGame()==Board.PLAYERWINS){  //player wins
+                        Toast.makeText(view.getContext(),"You Win",Toast.LENGTH_SHORT).show();
+                    }
+                    else if(newGame.getTurn()>Board.TOTALMOVES){    //draw
+                        Toast.makeText(view.getContext(),"Game Draw",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    void init(){
+        for(int i=0;i<GameMoveButton.length;i++){               //init boardButtons
+            GameMoveButton[i]=findViewById(BUTTON_IDS[i]);
+        }
+    }
+    int findIndexOf(Button button){
+        for(int i=0;i<BUTTON_IDS.length;i++){
+            if(button.getId()==BUTTON_IDS[i])
+                return i;
+        }
+        return -1;
     }
 }
---
+++ app/src/main/java/com/project/raj/tictactoe/GameUI.java
@@ -0,0 +1,60 @@
+package com.project.raj.tictactoe;
+
+import TicTacToe.AI;
+import TicTacToe.Board;
+/**
+ * Created by Rajvaibhav D. Rahane on 12-Jul-18.
+ */
+public class GameUI {
+    private Board board;
+    private AI ai;
+    private int turn;
+    GameUI(){
+        board=new Board();
+        ai=new AI();
+        turn=0;
+    }
+    public int playMove(int i){
+        turn+=2;
+        board.setBoardIndex(i,board.getPlayer());
+        int movePlayed=-1;
+        if(turn<= Board.TOTALMOVES){
+            switch (turn){
+                case 2:{
+                    if(!board.isBlank(4)){                        //center taken
+                        movePlayed=ai.getRandomCorner()-1;
+                        board.setBoardIndex(movePlayed, board.getComputer());//take any corner
+                    }
+                    else if(!board.isBlank(0)||!board.isBlank(2)||!board.isBlank(6)||!board.isBlank(8)){    //corner taken
+                        movePlayed=4;
+                        board.setBoardIndex(movePlayed, board.getComputer());                               //take center
+                    }
+                    else{                                   //edge taken
+                        if(!board.isBlank(1)||!board.isBlank(5)){
+                            movePlayed=2;
+                            board.setBoardIndex(movePlayed, board.getComputer());
+                        }
+                        else{
+                            movePlayed=6;
+                            board.setBoardIndex(movePlayed, board.getComputer());
+                        }
+                    }
+                    break;
+                }
+                case 4:
+                case 6:
+                case 8:{
+                    movePlayed=ai.findBestMove(board);
+                    board.setBoardIndex(movePlayed, board.getComputer());
+                }
+            }
+        }
+        return movePlayed;
+    }
+    public int getTurn(){
+        return turn;
+    }
+    public int evaluateGame(){
+        return board.evaluateBoard(board.getBoard());
+    }
+}
